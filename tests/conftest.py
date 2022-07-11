b4fe93a33a1dbfa0d2122d784388a3f00e07dddd
Ricardo Cisterna <r.cisternasantos@gmail.com>
2022-07-11 10:30:53 -0400

    test: add performance test

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,10 @@ import pytest
 from requests_tracker import Tracker
 
 
+def get_random_ip():
+    return socket.inet_ntoa(struct.pack(">I", random.randint(1, 0xFFFFFFFF)))
+
+
 @pytest.fixture
 def reset_tracker():
     Tracker.clear()
@@ -13,4 +17,4 @@ def reset_tracker():
 
 @pytest.fixture
 def random_ip():
-    return socket.inet_ntoa(struct.pack(">I", random.randint(1, 0xFFFFFFFF)))
+    return get_random_ip()
--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,4 +1,9 @@
+import time
+
+import pytest
 from requests_tracker import Tracker
+
+from .conftest import get_random_ip
 
 
 def test_add_single_ip(reset_tracker, random_ip):
@@ -22,3 +27,23 @@ def test_clear(reset_tracker, random_ip):
     assert Tracker._counts_by_addr == {}
     assert Tracker._addrs_by_count == {}
     assert Tracker._sorted_counts == []
+
+
+@pytest.mark.parametrize(
+    "counts_by_ips",
+    [
+        {get_random_ip(): 1000, get_random_ip(): 10000, get_random_ip(): 5000},
+        {get_random_ip(): i * 100000 for i in range(1, 21)},
+    ],
+)
+def test_performance(reset_tracker, counts_by_ips):
+    for ip, count in counts_by_ips.items():
+        for _ in range(count):
+            Tracker.request_handled(ip)
+    start = time.time()
+    Tracker.top100()
+    end = time.time()
+    assert Tracker._counts_by_addr == counts_by_ips
+    assert Tracker._addrs_by_count == {c: [ip] for ip, c in counts_by_ips.items()}
+    assert Tracker._sorted_counts == sorted(counts_by_ips.values(), reverse=True)
+    assert end - start < 0.3
